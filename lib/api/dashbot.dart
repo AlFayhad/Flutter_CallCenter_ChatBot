f5f12e4e20a8819712e65f3df88a1dd699dfe31f
AlFayhad <malfayhad@gmail.com>
2022-12-18 00:06:53 +0700

    delete videos api

--- lib/api/dashbot.dart
+++ lib/api/dashbot.dart
@@ -9,12 +9,12 @@ class Dashbot {
     'What\'s a pirate\'s favourite programming language? R.',
     'What do you call fake spaghetti? ... An impasta!',
   ];
-  List<String> videos = [
-    'https://www.youtube.com/watch?v=2Cl0C-9dK48',
-    'https://www.youtube.com/watch?v=Ikg7vHJoZjA',
-    'https://www.youtube.com/watch?v=sa_U0jffQII',
-    'https://www.youtube.com/watch?v=8-szcYzFVao'
-  ];
+  // List<String> videos = [
+  //   'https://www.youtube.com/watch?v=2Cl0C-9dK48',
+  //   'https://www.youtube.com/watch?v=Ikg7vHJoZjA',
+  //   'https://www.youtube.com/watch?v=sa_U0jffQII',
+  //   'https://www.youtube.com/watch?v=8-szcYzFVao'
+  // ];
 
   Message getResponseMessage(Message input) {
     if (input.message.contains('joke')) {
@@ -27,12 +27,13 @@ class Dashbot {
           sender: 'Dash',
           message:
               'Today is ${DateFormat('EEEE, MMMM d').format(DateTime.now())}');
-    } else if (input.message.contains('video')) {
-      return Message(
-          sender: 'Dash',
-          message:
-              'Check out this video from the Flutter channel! ${videos[Random().nextInt(videos.length)]}');
-    }
+    } 
+    // else if (input.message.contains('video')) {
+    //   return Message(
+    //       sender: 'Dash',
+    //       message:
+    //           'Check out this video from the Flutter channel! ${videos[Random().nextInt(videos.length)]}');
+    // }
 
     return Message(
       sender: 'Dash',
